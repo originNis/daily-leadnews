8de164951cb5a3ca521c77f99be1881115e7f1e0
originNis <holyrybin@foxmail.com>
2024-05-25 21:09:17 +0800

    Kafka：引入kafka组件，消息的发送与接受测试成功
    Signed-off-by: originNis <holyrybin@foxmail.com>

---
+++ heima-leadnews-test/kafka-demo/src/main/java/ConsumerQuickStart.java
@@ -0,0 +1,47 @@
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Properties;
+
+/**
+ * @author: Rybin
+ * @description:
+ * @date: 2024/5/25
+ */
+public class ConsumerQuickStart {
+    public static void main(String[] args) {
+        Properties properties = new Properties();
+        // kafka监听接口
+        properties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "124.223.106.233:9092");
+
+        // 反序列化器
+        properties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringDeserializer");
+        properties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringDeserializer");
+
+        /**
+         * 设置组。
+         * 当多个消费者在同一个组时，同一条消息只会被一个消费者获取，
+         * 当消费者在不同组时，同一条消息会被不同组都获取。
+         */
+        properties.put(ConsumerConfig.GROUP_ID_CONFIG, "group-test");
+
+        // 创建消费者
+        KafkaConsumer consumer = new KafkaConsumer(properties);
+
+        // 订阅主题，主题可以是多个
+        consumer.subscribe(Collections.singletonList("topic-1"));
+
+        while (true) {
+            ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(1000));
+            for (ConsumerRecord<String, String> record : records) {
+                System.out.println(record.key());
+                System.out.println(record.value());
+            }
+        }
+    }
+}
---
+++ heima-leadnews-test/kafka-demo/src/main/java/ProducerQuickStart.java
@@ -0,0 +1,33 @@
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+
+import java.util.Properties;
+
+/**
+ * @author: Rybin
+ * @description:
+ * @date: 2024/5/25
+ */
+public class ProducerQuickStart {
+    public static void main(String[] args) {
+        Properties properties = new Properties();
+        // kafka监听端口
+        properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "124.223.106.233:9092");
+        // 序列化器
+        properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
+        properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
+
+        // 根据配置创建kafka
+        KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
+
+        // 创建消息
+        ProducerRecord<String, String> producerRecord =
+                new ProducerRecord<String, String>("topic-1", "key-1", "success!");
+        // 发送消息
+        producer.send(producerRecord);
+
+        // 关闭通道才能成功发送
+        producer.close();
+    }
+}
